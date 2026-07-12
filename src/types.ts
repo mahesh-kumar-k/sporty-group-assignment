5149274190194e18e3304603a530f0491738edab
Mahesh <maheshkumarkorukonda@gmail.com>
2026-07-12 17:41:09 +0530

    Updates

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-export interface League {
-  idLeague: string;
-  strLeague: string;
-  strSport: string;
-  strLeagueAlternate: string;
-}
-
-export type BadgeMap = Record<string, string | null | undefined>;
